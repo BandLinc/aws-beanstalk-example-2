1dafa2deb239e2c932f057d844a1de174762d216
LincolnLidanya <lincolnlidanya@gmail.com>
2021-03-08 12:21:18 +0300

    Puhsing the first commit

---
+++ src/main/java/com/primers/aws/HelloController.java
@@ -0,0 +1,12 @@
+package com.primers.aws;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping ("/hello")
+@RestController
+public class HelloController {
+    public String hello () {
+        return "Hello Amazon";
+    }
+}
